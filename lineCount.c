1e2011c08f4267e30fed8df71cb8004546ccbcfb
Mike Kazzi <msk0693@yahoo.com>
2019-12-25 18:48:17 -0500

    Add files via upload

---
+++ lineCount.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+main(){
+	int c, b, t, nl;
+	nl = 0 ;
+	while(getchar() != EOF){
+		if (c == '\n')
+			++nl;
+		if (c == '\t')
+			++t;
+		if (c == space)
+			++b;
+	}
+	printf("%d\n %d\n %d\n", nl, t, b);
+	}	
